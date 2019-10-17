f109062ff18ad189b6ad557c87925039ace0921a
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-10-17 01:16:01 -0300

    halfway done with this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window/global object binding - when none of the other rules apply and not in strict mode, the this keyword points to the window or the object console
+* 2. implicit binding - the value of this is the object preceding the dot
+* 3. new binding - refers to the instance of the object created by the constructor function 
+* 4. explicit binding - when call or apply method is used
 *
 * write out a code example of each explanation above
 */
@@ -13,9 +13,28 @@
 
 // code example for Window Binding
 
+function greet(person) {
+    console.log(this);
+    return `Hello, ${person}`;
+}
+
+greet('Jevon');
+
 // Principle 2
 
 // code example for Implicit Binding
+let singer = {
+    name: 'Lil Nas X',
+    hometown: 'Atlanta',
+    lyrics: 'I\'m gonna take my horse to the Old Town Road',
+    sing: function() {
+        console.log(this.lyrics);
+    }
+}
+
+singer.sing();
+
+
 
 // Principle 3
 
@@ -24,3 +43,15 @@
 // Principle 4
 
 // code example for Explicit Binding
+let singer = {
+    name: 'Lil Nas X',
+    hometown: 'Atlanta',
+    lyrics: 'I\'m gonna take my horse to the Old Town Road',
+}
+
+function sing() {
+    console.log(this.lyrics);
+}
+
+sing.call(singer);
+
